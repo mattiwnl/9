657b5db6cec5e30083497d9fa60bc858c8721d5d
Matthew McInnis <46408819+mattiwnl@users.noreply.github.com>
2020-02-21 10:22:05 -0600

    Update Robot.java

--- finalCode/src/main/java/frc/robot/Robot.java
+++ finalCode/src/main/java/frc/robot/Robot.java
@@ -95,7 +95,12 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+        // schedule the autonomous command (example)
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.schedule();
+        }
     }
 
     @Override
@@ -104,6 +109,9 @@ public class Robot extends TimedRobot {
     
     @Override
     public void teleopInit() {
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
     }
 
     @Override
